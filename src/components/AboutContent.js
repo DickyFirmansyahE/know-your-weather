dfe71b0e8a33624162622f30abecff6f79e9a769
Nflans10 <89309988+AnsNaufal@users.noreply.github.com>
2022-11-27 23:48:19 +0700

    Add files via upload

---
+++ src/components/AboutContent.js
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Paper from "@mui/material/Paper";
+import Tooltip from '@mui/material/Tooltip';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
+import profile from '../services/profile-data';
+
+export default function MediaCard() {
+  return (
+    <div class="paper-container">
+    <Paper 
+        sx={{ p: "2px 14px", display: "flex", flexDirection: "column", alignItems: "center", width: "100%", backgroundColor: "#ffffffbe", borderRadius: "20px", boxShadow: "1px 2px #00000063" }}>
+    <h1 className="text-title">WHO WE ARE</h1>
+    <Typography variant="body2" color="text.secondary" sx={{marginTop:"50px", fontSize:"18px", fontWeight:"bold"}}>Kami dari Tim Capstone C22-062 yang terbentuk karena sebuah tugas project akhir dari sebuah program Kampus Merdeka SIB Batch 3 dengan Mitra Dicoding. Dengan adanya website ini dapat membantu dan memfasilitasi pengguna agar mengetahui keadaan cuaca di daerah pengguna berada, sehingga dapat mengantisipasi keadaan untuk kedepannya.</Typography>
+    <div className="AboutUs">
+    {profile.map((profile) => (
+    <Card sx={{ backgroundColor: '#00000046', borderRadius: "25px", padding: "20px 30px", marginBottom: "50px !important",  margin: "0 10px", marginTop: "50px"}}>
+      <Grid display="flex" justifyContent="center" alignItems="center">
+      <CardMedia sx={{borderRadius: "100px", maxWidth:"155px", marginTop:"30px !important"}}
+        component="img"
+        height="155"
+        image= {profile.photo}
+        alt=""
+      />
+      </Grid>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {profile.nama}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{fontSize:"15px", marginTop:"20px"}}>
+        {profile.text}
+        </Typography>
+      </CardContent>
+      <CardActions>
+      <Grid container justifyContent="center">
+      <Tooltip title="LinkedIn">
+      <Link href={profile.socialMedia} target="_blank" rel="noopener">
+      <IconButton> 
+      <LinkedInIcon  sx={{ fontSize: 40 }} />
+      </IconButton>
+      </Link>
+      </Tooltip>
+      </Grid>
+      </CardActions>
+    </Card>
+    ))}
+    </div>
+    </Paper>
+    </div>
+  );
+}
