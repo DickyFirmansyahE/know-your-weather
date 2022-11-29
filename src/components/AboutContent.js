88755cf445c08f410fbb9c9d58f2c9a1b673bc06
Dicky Firmansyah Ekadiputra <109281377+DickyFirmansyahE@users.noreply.github.com>
2022-11-29 12:09:30 +0000

    edit some code

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -14,14 +14,16 @@ import profile from '../services/profile-data';
 
 export default function AboutContent() {
   return (
-    <div class="paper-container">
+    <div className="paper-container">
     <Paper 
-        sx={{ p: "2px 14px", display: "flex", flexDirection: "column", alignItems: "center", width: "100%", backgroundColor: "#ffffffbe", borderRadius: "20px", boxShadow: "1px 2px #00000063" }}>
-    <h1 className="text-title">WHO WE ARE</h1>
-    <Typography variant="body2" color="text.secondary" sx={{marginTop:"50px", fontSize:"18px", fontWeight:"bold"}}>Kami dari Tim Capstone C22-062 yang terbentuk karena sebuah tugas project akhir dari sebuah program Kampus Merdeka SIB Batch 3 dengan Mitra Dicoding. Dengan adanya website ini dapat membantu dan memfasilitasi pengguna agar mengetahui keadaan cuaca di daerah pengguna berada, sehingga dapat mengantisipasi keadaan untuk kedepannya.</Typography>
+        sx={{ padding: "14px 12px", display: "flex", flexDirection: "column", alignItems: "center", width: "100%", backgroundColor: "#ffffffbe", borderRadius: "20px", boxShadow: "1px 2px #00000063" }}>
+    <h1 className="text-title">WHO W<span className='text-title2'>E ARE</span></h1>
+    <Typography variant="body2" color="text.secondary" sx={{marginTop:"20px", fontSize:"18px", fontWeight:"bold"}}>Kami dari Tim Capstone C22-062 yang terbentuk karena sebuah tugas project akhir dari sebuah program Kampus Merdeka SIB Batch 3 dengan Mitra Dicoding. Dengan adanya website ini dapat membantu dan memfasilitasi pengguna agar mengetahui keadaan cuaca di daerah pengguna berada, sehingga dapat mengantisipasi keadaan untuk kedepannya.</Typography>
     <div className="AboutUs">
     {profile.map((profile) => (
-    <Card sx={{ backgroundColor: '#00000046', borderRadius: "25px", padding: "20px 30px", marginBottom: "50px !important",  margin: "0 10px", marginTop: "50px"}}>
+    <Card 
+      key={profile.id}
+      sx={{ backgroundColor: '#00000046', borderRadius: "25px", padding: "20px 30px",  margin: "0 10px", marginTop: "20px"}}>
       <Grid display="flex" justifyContent="center" alignItems="center">
       <CardMedia sx={{borderRadius: "100px", maxWidth:"155px", marginTop:"30px !important"}}
         component="img"
