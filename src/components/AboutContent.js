8990faa102328a82060e1be1bad2272de48dc072
Nflans10 <89309988+AnsNaufal@users.noreply.github.com>
2022-12-02 17:35:20 +0000

    edit about us

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -15,46 +15,46 @@ import profile from '../services/profile-data';
 export default function AboutContent() {
   return (
     <div className="paper-container">
-    <Paper 
+      <Paper
         sx={{ padding: "14px 12px", display: "flex", flexDirection: "column", alignItems: "center", width: "100%", backgroundColor: "#ffffffbe", borderRadius: "20px", boxShadow: "1px 2px #00000063" }}>
-    <h1 className="text-title">WHO W<span className='text-title2'>E ARE</span></h1>
-    <Typography variant="body2" color="text.secondary" sx={{marginTop:"20px", fontSize:"18px", fontWeight:"bold"}}>Kami dari Tim Capstone C22-062 yang terbentuk karena sebuah tugas project akhir dari sebuah program Kampus Merdeka SIB Batch 3 dengan Mitra Dicoding. Dengan adanya website ini dapat membantu dan memfasilitasi pengguna agar mengetahui keadaan cuaca di daerah pengguna berada, sehingga dapat mengantisipasi keadaan untuk kedepannya.</Typography>
-    <div className="AboutUs">
-    {profile.map((profile) => (
-    <Card 
-      key={profile.id}
-      sx={{ backgroundColor: '#00000046', borderRadius: "25px", padding: "20px 30px",  margin: "0 10px", marginTop: "20px"}}>
-      <Grid display="flex" justifyContent="center" alignItems="center">
-      <CardMedia sx={{borderRadius: "100px", maxWidth:"155px", marginTop:"30px !important"}}
-        component="img"
-        height="155"
-        image= {profile.photo}
-        alt=""
-      />
-      </Grid>
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-        {profile.nama}
-        </Typography>
-        <Typography variant="body2" color="text.secondary" sx={{fontSize:"15px", marginTop:"20px"}}>
-        {profile.text}
-        </Typography>
-      </CardContent>
-      <CardActions>
-      <Grid container justifyContent="center">
-      <Tooltip title="LinkedIn">
-      <Link href={profile.socialMedia} target="_blank" rel="noopener">
-      <IconButton> 
-      <LinkedInIcon  sx={{ fontSize: 40 }} />
-      </IconButton>
-      </Link>
-      </Tooltip>
-      </Grid>
-      </CardActions>
-    </Card>
-    ))}
-    </div>
-    </Paper>
+        <h1 className="text-title">WHO W<span className='text-title2'>E ARE</span></h1>
+        <Typography variant="body2" color="text.secondary" sx={{ marginTop: "20px", fontSize: "18px", fontWeight: "bold" }}>Kami merupakan Tim Capstone C22-062 membuat sebuah website ini untuk dapat membantu dan memfasilitasi user agar mengetahui keadaan cuaca di daerah user berada, sehingga dapat mengantisipasi keadaan untuk kedepannya.</Typography>
+        <div className="AboutUs">
+          {profile.map((profile) => (
+            <Card
+              key={profile.id}
+              sx={{ backgroundColor: '#00000046', borderRadius: "25px", padding: "20px 30px", margin: "0 10px", marginTop: "20px" }}>
+              <Grid display="flex" justifyContent="center" alignItems="center">
+                <CardMedia sx={{ borderRadius: "100px", maxWidth: "155px", marginTop: "30px !important" }}
+                  component="img"
+                  height="155"
+                  image={profile.photo}
+                  alt=""
+                />
+              </Grid>
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  {profile.nama}
+                </Typography>
+                <Typography variant="body2" color="text.secondary" sx={{ fontSize: "15px", marginTop: "20px" }}>
+                  {profile.text}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Grid container justifyContent="center">
+                  <Tooltip title="LinkedIn">
+                    <Link href={profile.socialMedia} target="_blank" rel="noopener">
+                      <IconButton>
+                        <LinkedInIcon sx={{ fontSize: 40 }} />
+                      </IconButton>
+                    </Link>
+                  </Tooltip>
+                </Grid>
+              </CardActions>
+            </Card>
+          ))}
+        </div>
+      </Paper>
     </div>
   );
 }
--- src/services/profile-data.js
+++ src/services/profile-data.js
@@ -1,32 +1,32 @@
 const profile = [
-    {
-      id: 1,
-      photo: "./andi.jpg",
-      nama: "Andi Paris Bachtiar",
-      text: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-      socialMedia: "https://www.linkedin.com/in/andi-paris-b-871960141/"
-    },
-    {
-      id: 2,
-      photo: "./azmiy.jpeg",
-      nama: "Azmiy Thuffail",
-      text: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-      socialMedia:"https://www.linkedin.com/in/azmiy-thufail/"
-    },
-    {
-      id: 3,
-      photo: "./dicky.jpg",
-      nama: "Dicky Firmansyah",
-      text: "Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica",
-      socialMedia:"https://www.linkedin.com/in/dicky-firmansyah-ekadiputra-71001b22b/"
-    },
-    {
-      id: 4,
-      photo: "./naufal.jpg",
-      nama: "Naufal Anas",
-      text: "If you want live a happy life, tie it to a goal, not to people or things.",
-      socialMedia:"https://www.linkedin.com/in/naufal-anas-3b2333204/"
-    }
-  ];
+  {
+    id: 1,
+    photo: "./andi.jpg",
+    nama: "Andi Paris Bachtiar",
+    text: "Jangan pernah berhenti untuk belajar karena itulah yang akan mendorongmu untuk terus bergerak maju dan menjadi lebih baik",
+    socialMedia: "https://www.linkedin.com/in/andi-paris-b-871960141/"
+  },
+  {
+    id: 2,
+    photo: "./azmiy.jpeg",
+    nama: "Azmiy Thuffail",
+    text: "Do something today that your future self will thank you for.",
+    socialMedia: "https://www.linkedin.com/in/azmiy-thufail/"
+  },
+  {
+    id: 3,
+    photo: "./dicky.jpg",
+    nama: "Dicky Firmansyah",
+    text: "It does not matter how slowly you go as long as you do not stop.",
+    socialMedia: "https://www.linkedin.com/in/dicky-firmansyah-ekadiputra-71001b22b/"
+  },
+  {
+    id: 4,
+    photo: "./naufal.jpg",
+    nama: "Naufal Anas",
+    text: "If you want live a happy life, tie it to a goal, not to people or things.",
+    socialMedia: "https://www.linkedin.com/in/naufal-anas-3b2333204/"
+  }
+];
 
 export default profile;
