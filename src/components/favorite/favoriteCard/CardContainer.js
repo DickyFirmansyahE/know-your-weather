5319f240410f492b77094f150dbb0fe5f655c7c2
Dicky Firmansyah Ekadiputra <109281377+DickyFirmansyahE@users.noreply.github.com>
2022-12-06 23:48:34 +0700

    deleting old files

--- src/components/favorite/favoriteCard/CardContainer.js
+++
@@ -1,9 +0,0 @@
-import * as actions from '../../../redux/actions';
-import { connect } from 'react-redux';
-import CardComponent from './Card';
-
-const mapDispatchToProps = dispatch => ({
-  onDelete: data => dispatch(actions.deleteCard(data)),
-});
-
-export default connect(null, mapDispatchToProps)(CardComponent);
